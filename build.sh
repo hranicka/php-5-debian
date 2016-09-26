14b0d5eb0b3b40745cb9c16e55858c8ff0ebaa1d
Jaroslav Hranička <hranicka@outlook.com>
2016-09-26 11:21:41 +0200

    bison 2.7 https://bugs.php.net/bug.php?id=69055

--- build.sh
+++ build.sh
@@ -8,7 +8,6 @@ sudo apt-get install -y \
     pkg-config \
     git-core \
     autoconf \
-    bison \
     libxml2-dev \
     libbz2-dev \
     libmcrypt-dev \
@@ -21,6 +20,26 @@ sudo apt-get install -y \
     libpspell-dev \
     libreadline-dev \
     re2c
+
+
+# https://bugs.php.net/bug.php?id=69055
+sudo apt-get purge bison
+
+sudo apt-get install -y build-essential m4
+
+wget http://ftp.gnu.org/gnu/bison/bison-2.7.tar.gz
+tar -xvf bison-2.7.tar.gz
+rm bison-2.7.tar.gz
+cd bison-2.7
+
+./configure --prefix=/usr/local/bison --with-libiconv-prefix=/usr/local/libiconv/
+
+make
+sudo make install
+
+sudo ln -sf /usr/local/bison/bin/bison /usr/bin/bison
+sudo ln -sf /usr/local/bison/bin/yacc /usr/bin/yacc
+
 
 sudo mkdir /usr/local/php5
 
