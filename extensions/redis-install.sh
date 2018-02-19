7210209a7246f4f1fa24543cdc1622ad9ece8f12
Jaroslav Hranička <me@hranicka.cz>
2018-02-19 12:37:12 +0100

    PHP 5.6.33

--- extensions/redis-install.sh
+++ extensions/redis-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
-/usr/local/php5/bin/pecl -C /usr/local/php5/etc/pear.conf install redis-2.2.8
+/usr/local/php5/bin/pecl -C /usr/local/php5/etc/pear.conf install redis-3.1.6
 
 # Add config files
 echo "extension=redis.so" | sudo tee -a /usr/local/php5/etc/conf.d/modules.ini
